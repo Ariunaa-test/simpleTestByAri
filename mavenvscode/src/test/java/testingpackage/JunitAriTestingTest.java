eac9cd8731460d3f67043c17ce7d416200d7e404
Ariunaa.Bertelsen@smhi.se <ariunaa.bertelsen@smhi.se>
2021-03-17 00:59:01 +0100

    test will not fail

--- mavenvscode/src/test/java/testingpackage/JunitAriTestingTest.java
+++ mavenvscode/src/test/java/testingpackage/JunitAriTestingTest.java
@@ -35,7 +35,7 @@ public class JunitAriTestingTest {
         
         numberPrinting.printNumber(intArray);
         
-        Assert.assertEquals("1,2,3,4,5,6,7,8,9,10,", outputStreamCaptor.toString()
+        Assert.assertEquals("1,2,3,4,5,6,7,8,9,10", outputStreamCaptor.toString()
             .trim());
     
     }
